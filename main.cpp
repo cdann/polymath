4f73b370f74b4b0c02f1b1b8084244da0be6acc6
Celine DANNAPPE <cdannapp@e2r2p5.42.fr>
2015-05-06 17:57:19 +0200

    gestion des options

--- main.cpp
+++ main.cpp
@@ -3,18 +3,65 @@
 #include <string>
 #include <iostream>
 
+bool	put_option(int argc, char const *argv[])
+{
+	int len;
+
+	for (int i = 1; i < argc - 1; i++)
+	{
+		if (argv[i][0] != '-')
+			return false;
+		len = 0;
+		while (argv[i][len] != '\0')
+			len++;
+		if (len != 2)
+		{
+			std::cout << "false " << len << std::endl;
+
+			return false;
+		}
+		switch (argv[i][1])
+		{
+			//case 'd':
+			//	Parser::setDebug = true;
+			//	break;
+			//case
+
+			case 'f':
+				Poly::setFactor();
+				break;
+			//case
+		}
+	}
+	return true;
+}
+
+void usage(char const *name)
+{
+	std::cout << "usage: " << name << "monequation"<< std::endl;
+	std::cout << "options: -f to factor form"<< std::endl;
+
+
+}
+
+
 int main(int argc, char const *argv[])
 {
-	if (argc == 2)
+	if (argc >= 2)
 	{
 		Parser p;
-		p.parse(argv[1]);
-		p.test();
+		if (!put_option(argc, argv))
+		{
+			usage(argv[0]);
+			return 0;
+		}
+		p.parse(argv[argc-1]);
+		//p.test();
 		Poly calcul(p);
 	}
 	else
 	{
-		std::cout << "usage: " << argv[0] << "monequation"<< std::endl;
+		usage(argv[0]);
 	}
 	return 0;
 }
